c98a57d6de990ee54077772d394d7fe4b18c0068
briansnelson <brian@grit360.com>
2021-06-15 19:26:14 -0700

    updated pseudo code file

--- js/app.js
+++ js/app.js
@@ -14,18 +14,16 @@ let UserProfile = function(name, timer, logs) {
 
 // Take in ID from HTML
 // DOM to create a form
-// Display name in form
+// Populate the form on homepage/DOM to capture data
 
 
 // Take in information
 // If statement -- If local storage present, do not populate form
-//      Make a new form to only display breathing timer
+// 
 // Set it so the user must enter in information
-// have two seperate forms to take in inforamtion(name)
-// second form???
+
 // Reference (name, time) through constructor function
-// Time is a parameter into timer function(number must be 30, 60, 90, or 120?)
-//     Drop down Menu of numbers
+
 // Click eventListener button
 // Record that information through local storage
 // Make sure to set through JSON of stringify
@@ -36,21 +34,21 @@ let UserProfile = function(name, timer, logs) {
 
 // Display Greeting
 // this.name + time() save to variable
-// Display variable through DOM on page through an ID
-// Remove greeting after timer is done
+// Greeting Function: Display variable(name)through DOM on HTML page through an ID 
+// *** Stretch goal 30 second
+
+// Button to start timer, remove greeting
 
 // Notes:
 // 'required' -- place in form in HTML
 // event.preventDefault(); -- makes it so information stays on the form
 //    remove this after form works
 
-
-
-
-
-
-
-
+// Button to load Readings & Reflections page
+//Function to allow next chapter of Tao to load
+//Save reflections in local storage
+//Clear reflection page once submitted for next entry
+//
 
 // Timer function
 
