d58c9d98ee068670fd0eadd6883837a759c5a641
Mark Thanadabouth <mark28ten@gmail.com>
2021-06-15 20:39:35 -0700

    finished sudo code

--- js/app.js
+++ js/app.js
@@ -50,8 +50,20 @@ let UserProfile = function(name, timer, logs) {
 //Clear reflection page once submitted for next entry
 //
 
-// Timer function
+// When recording each log on Zen page
+// Attach index's to each (refer to lab 14b shopping cart)
+// display 2 tables
+// Left table will have past Logs
+// Right table will display logs in detail
+// Attach EventListeners with their ID on each log on the left
+// when eventListeners(logs) are clicked display that log with quotes on right side
+// Each log will have an index
+// It will display the whole log with which quote(s) they were written next to(which were seen by user when they clicked save log)
+// Remove that eventListener when log is shown
+// Either have a close function to close out detailed log, or when clicking on a new log
+//      will re-populate the right table with the new log
 
+// Timer function
 
 
 
